d0f81e038e944074abbc378276461a8b8619818a
Caleb Lucas <t2drhymes@gmail.com>
2019-07-08 18:01:31 +0100

    stop printing withdraw amount to console

--- client/src/utils/dapp/accountWorker.js
+++ client/src/utils/dapp/accountWorker.js
@@ -25,7 +25,6 @@ export const withdrawFunds = amt => {
                 // Get contract instance
                 const instance = await PurchaseManager.deployed();
                 // Get user balance
-                console.log(amt)
                 await instance.withdrawFunds(amt, { from: addr });
                 resolve();
             } catch (e) {
